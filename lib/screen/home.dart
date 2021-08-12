c2a9f360f85f36adda225910ffef9c982cdf86de
Ahmad Amirulhafiz <mohdnor.ahmadamirulhafiz@etiqa.com.my>
2021-08-12 08:39:12 +0800

    remove header name

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -55,7 +55,7 @@ class _HomeState extends State<Home> {
                         padding: const EdgeInsets.all(4.0),
                         child: Center(
                           child: Text(
-                            "monomics",
+                            "",
                             style:
                                 cmFont(33, color: Colors.white, adapt: false),
                           ),
